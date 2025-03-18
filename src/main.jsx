bb8bb6a64f7b42c5f37d6fc9c3489e0556dc3356
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-03-17 20:10:24 -0600

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { JournalApp } from './JournalApp'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <JournalApp />
+  </StrictMode>,
+)
