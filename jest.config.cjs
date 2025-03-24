3b011595c56c6f0bc54998367f40888dcfa17289
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-03-23 20:08:45 -0600

    configuración env

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js'],
-    transformIgnorePatterns: [],
+    transformIgnorePatterns: [
+        //"node_modules/(?!(query-string|decode-uri-component|split-on-first|filter-obj)/)",
+      ],
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 // En caso de necesitar la implementación del FetchAPI
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+
+require('dotenv').config({
+    path: '.env.local'
+});
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+    getEnvironments: () => ({...process.env})
+}));
+
